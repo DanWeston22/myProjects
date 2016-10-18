b3a460192a231bb7677b9fcbbd6a9e0b52d7d339
Dan Weston <dan.weston22@googlemail.com>
2016-10-18 09:59:56 +0100

    Programme gets Forename & Surname (separately) from the command line. Then concentrates into full name and prints result to the command line.

---
+++ Strings/Strings/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Strings
+//
+//  Created by Dan Weston on 18/10/2016.
+//  Copyright © 2016 Dan Weston. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
