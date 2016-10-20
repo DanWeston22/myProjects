edfcb6f4d5813b7446e803170d53661307313ebd
Dan Weston <dan.weston22@googlemail.com>
2016-10-20 22:15:49 +0100

    Created CW1 Project File

---
+++ cw1_DanWeston/cw1_DanWeston/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  cw1_DanWeston
+//
+//  Created by Dan Weston on 20/10/2016.
+//  Copyright © 2016 Dan Weston. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    printf ("<time in milliseconds> <MIDI note number>\n"); //Prints format that user should input MIDI & Timing information.
+    
+    
+    return 0;
+}
