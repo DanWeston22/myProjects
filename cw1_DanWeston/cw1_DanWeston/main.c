c133299d6ba4ede97a4a9c5c75de949d2830bb0f
Dan Weston <dan.weston22@googlemail.com>
2016-10-30 11:50:41 +0000

    Tidied up code & documentation

--- cw1_DanWeston/cw1_DanWeston/main.c
+++ cw1_DanWeston/cw1_DanWeston/main.c
@@ -14,108 +14,107 @@
 
 int main(int argc, const char * argv[]) {
 
-    const int sampleFrequency = 48000.0;//Sets sampleFrequency to 48kHz
-    float timingValues[100];//Stores up to 100 values in timingValue array
-    float midiValues[100];//Stores up to 100 values in timingValue array
-    float timingInputValue;
-    float midiInputValue;
-    
-    if (argc == 2){//Checks to see if user enters '-help'
+    //'-help' Information.
+    if (argc == 2){
+        //Check if user inputed '-help'. If they did prints information.
         if (strcmp(argv[1], "-help") == 0){
-            printf("Please enter Timing Information and MIDI Note Numbers in the following format:\n<time in milliseconds> <MIDI note number>\n");//Prints format that user should input MIDI & Timing information.
+            printf("Please enter Timing Information and MIDI Note Numbers in the following format:\n<time in milliseconds> <MIDI note number>.\n");
             return 1;
         }
+        //If they didn't prints error message and ends program.
         else {
             printf("User input not in a recognised format\n");
             return 1;
         }
     }
+    //Arrays to store input data.
+    float timingValues[100];
+    float midiValues[100];
     
-    int numberOfInputLines = 0;//Sets timingCount to zero
-    for (numberOfInputLines = 0; numberOfInputLines < 100; numberOfInputLines++){//Counts values added to the 1st column of the values array
+    //Variables for user input.
+    float timingInputValue;
+    float midiInputValue;
+    int numberOfInputLines = 0;
+    
+    //Count number of input lines.
+    for (numberOfInputLines = 0; numberOfInputLines < 100; numberOfInputLines++){
         
+        //Gets input from command line.
         char *line = NULL;
         size_t len = 12;
         getline(&line, &len, stdin);
+        
+        //Variables for splitting up input data.
         char *token = NULL;
         const char space[] = " ";
         char *stopString;
-        int i = 0;
+        int inputCharater = 0;
 
-        for (i = 0; line[i] != 0; i++){
-            if (isalpha(line[i]) != 0) {
-                printf ("User input not in a recognised format.\n");//Prints error message when input isn't in the correct format
+        //Checks input for invalid data (charaters & punctuation) if found ends program.
+        for (inputCharater = 0; line[inputCharater] != 0; inputCharater++){
+            if ((isalpha(line[inputCharater]) != 0) || (ispunct(line[inputCharater]) && line[inputCharater]!='-')){
+                printf ("User input not in a recognised format\n");
                 return 2;
             }
         }
         
+        //Splits input data and saves first value to timingValue.
         token = strtok(line, space);
-        
-        for (i = 0; line[i] != 0; i++){
-            if (ispunct(token[i]) != 0){
-                printf ("User input not in a recognised format..\n");//Prints error message when input isn't in the correct format
-                return 2;
-            }
-        }
         timingInputValue = strtof(token, &stopString);
         
+        //Checks for a second value and saves. If there isn't a second value prints error and ends program.
         token = strtok(NULL, space);
         if (token == NULL){
-            printf ("User input not in a recognised format...\n");//Prints error message when input isn't in the correct format
+            printf ("User input not in a recognised format\n");
             return 2;
         }
-        for (i = 0; line[i] != 0; i++){
-            if ((ispunct(token[i]) && token[i]!='-')){
-                    printf ("User input not in a recognised format....\n");//Prints error message when input isn't in the correct format
-                    return 2;
-            }
-        }
-        
         midiInputValue = strtof(token, &stopString);
         
+        //If there is a third value prints error and ends program.
         token = strtok(NULL, space);
         if (token != NULL){
-            printf ("User input not in a recognised format!\n");//Prints error message when input isn't in the correct format
+            printf ("User input not in a recognised format\n");
             return 2;
         }
         
-        timingValues[numberOfInputLines] = timingInputValue;//Sets timingValue array with value [inputCount] timingInput value
+        //Saves timing and MIDI input values into the relevant arrays.
+        timingValues[numberOfInputLines] = timingInputValue;
+        midiValues[numberOfInputLines] = midiInputValue;
         
-        midiValues[numberOfInputLines] = midiInputValue;//Sets midiValue array with value [inputCount] midiInput value
-        
-        if (127 < midiInputValue) {//Checks if MIDIInput value is > 127
-            printf ("The MIDI ‘note on’ message contains data out of bounds\n");//Error message when MIDI data isn't valid
-            return 2;
+        //Checks that MIDI value is valid, if not prints error and ends program.
+        if (127 < midiInputValue) {
+            printf ("The MIDI ‘note on’ message contains data out of bounds\n");
+            return 3;
         }
         
-        if (timingValues[numberOfInputLines-1] > timingValues[numberOfInputLines]){//Checks if time values are increasing
-            printf("The time values need to be non-negative and increasing in value\n");//If not prints error
-            return 3;//Breaks out of loop and ends programme
+        //Checks that the timing values are increasing, if not prints error and ends program.
+        if (timingValues[numberOfInputLines-1] > timingValues[numberOfInputLines]){
+            printf("The time values need to be non-negative and increasing in value\n");
+            return 4;
         }
         
-        if (midiInputValue < 0 || numberOfInputLines == 99) {//Checks if MIDIInput value is < 0, or inputCount has readhed 100
+        //If MIDI value is < 0, or if the number of input lines has reached 100 breaks out of loop.
+        if (midiInputValue < 0 || numberOfInputLines == 99) {
             break;
         }
     }
 
-    for (int inputNumber = 1; inputNumber <= numberOfInputLines; ++inputNumber) {//For loop to cycle through input values
-            float duration = (timingValues[inputNumber] - timingValues[inputNumber-1]);//Calculates time to run from timingValues
-            float frequency = 440.0 * pow(2.0, ((midiValues[inputNumber-1] - 69.0)/12.0));//Calculates Frequency from midiValue
-            float omega = (2.0 * M_PI * frequency);//Sets omega to equal 2πf
-            float phase;//Sets phase to zero
+    //Loop to cycle through input values and calculate note duration, start time and frequency.
+    for (int inputNumber = 1; inputNumber <= numberOfInputLines; ++inputNumber) {
+            float duration = (timingValues[inputNumber] - timingValues[inputNumber-1]);
+            float frequency = 440.0 * pow(2.0, ((midiValues[inputNumber-1] - 69.0)/12.0));
             float startTime = (timingValues[inputNumber-1]);
-            
-            for (float currentTime; currentTime <= startTime + duration; currentTime += (1000.0/sampleFrequency)){//For loop for sample value calculation
-                float sampleValues = sin(phase);//Calculates the sample values
-                printf("%.6f\n", sampleValues);//Prints sample values
-                phase += (omega/sampleFrequency);//Adds 2πf/SR to phase
+            //Declarations
+            const int sampleFrequency = 48000.0;
+            float omega = (2.0 * M_PI * frequency);
+            float phase;
+        
+            //Loop calculates and prints sample values, and increments phase.
+            for (float currentTime; currentTime <= startTime + duration; currentTime += (1000.0/sampleFrequency)){
+                float sampleValues = sin(phase);
+                printf("%.6f\n", sampleValues);
+                phase += (omega/sampleFrequency);
             }
     }
     return 0;
 }
-//BUGS
-
-//If less or more than 2 numbers are entered on a line it doesnt give an error message (Try using fgets & sscanf or getline)
-
-//Code Complete - Layout
-
